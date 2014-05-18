dbb3a4b8f54d20aab9d8bde904ddc97f801ba9ac
Sindre Føring Devik <sindre.devik@gmail.com>
2014-05-18 23:23:43 +0200

    tree handles empty nodes, and direct endinsertion

--- lib/tree.dart
+++ lib/tree.dart
@@ -3,39 +3,46 @@ class Tree<T> {
   T content;
   List<Tree> children = [];
   
-  Tree<T> _Find(List<String> path) {
-    print(path.first);
-    print(identifier);
-    if (path.first == identifier) {
-      if (path.length == 1) {
-        print("return $this");
+  Tree<T> _walk(List<String> path, {bool makeIntermediate: false}) {
+    if (path.length == 1) {
+      if (path.first == identifier) {
         return this;
       } else {
-        try {
-          path.removeAt(0);
-          var child = children.singleWhere((child) => child.identifier == path.first);
-          return child.Find(path);
-        } catch (_) {}
+        throw("Tree: internal error: identifier is $identifier, want ${path.first}");
+      }
+    } else {
+      path.removeAt(0);
+      var match = children.where((e) => e.identifier == path.first);
+      switch (match.length) {
+        case 0:
+          if (makeIntermediate) {
+            var newTree = new Tree<T>(null, path.first);
+            children.add(newTree);
+            return newTree._walk(path, makeIntermediate: makeIntermediate);
+          }
+          return null;
+        case 1:
+          return match.first._walk(path, makeIntermediate: makeIntermediate);
+        default:
+          throw("Tree: internal error: several children with same identifier");
       }
     }
-    return null;
   }
-  T Find(List<String> path) {
-    var t = _Find(path);
+  T find(List<String> path) {
+    
+    var t = _walk(path);
     if (t == null) {
       return null;
     }
     return t.content;
   }
   
-  Tree<T> NewAt(List<String> path, T content) {
-    var t = _Find(path.sublist(0, path.length - 1));
-    if (t == null) {
-      return null;
-    }
-    var newTree = new Tree(content, identifier);
+  Tree<T> newAt(List<String> path, T content, {bool makeIntermediate: false}) {
+    var t = _walk(path.sublist(0, path.length - 1), makeIntermediate: makeIntermediate);
+    var newTree = new Tree(content, path.last);
     t.children.add(newTree);
     return newTree;
   }
+  
   Tree(this.content, this.identifier);
 }
