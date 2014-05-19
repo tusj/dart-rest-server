570061f265f4405815e00090af6d46df98bc5511
Sindre Føring Devik <sindre.devik@gmail.com>
2014-05-19 09:56:50 +0200

    added makeHttpHandler

---
+++ lib/handler.dart
@@ -0,0 +1,80 @@
+library handler;
+
+import 'dart:io';
+
+typedef void HttpRequestHandler(HttpRequest r);
+
+abstract class HttpHandler {
+  void Get(HttpRequest r);
+  void Post(HttpRequest r);
+  void Put(HttpRequest r);
+  void Delete(HttpRequest r);
+  void Head(HttpRequest r);
+}
+
+void HttpRequestDelegator(HttpHandler h, HttpRequest r) {
+  switch (r.method) {
+    case "GET":
+      h.Get(r);
+      break;
+    case "POST":
+      h.Post(r);
+      break;
+    case "PUT":
+      h.Put(r);
+      break;
+    case "DELETE":
+      h.Delete(r);
+      break;
+    case "HEAD":
+      h.Head(r);
+      break;
+    default:
+      throw("Unhandled Http Method: ${r.method}");
+  }
+}
+
+HttpRequestHandler MakeHttpRequestHandler(HttpHandler h) {
+  return (HttpRequest r) => HttpRequestDelegator(h, r);
+}
+class MongoHandler implements HttpHandler {
+  String res;
+  void Get(HttpRequest r) {
+    print('GET');
+
+    r.response
+      ..write(res)
+      ..close();
+  }
+  void Post(HttpRequest r) {
+    print('Post');
+
+    var newres = new StringBuffer();
+    r.listen((List<int> data) => 
+        newres.write(new String.fromCharCodes(data)),
+        onError: (error) {
+          r.response
+            ..write(error)
+            ..statusCode = HttpStatus.INTERNAL_SERVER_ERROR
+            ..close();
+        },
+        onDone: () {
+          res = newres;
+          r.response
+            ..close();
+        });
+  }
+  void Put(HttpRequest r) => Post(r);
+  void Delete(HttpRequest r) {
+    print('Delete');
+
+    res = "";
+  }
+  void Head(HttpRequest r) {
+    print('Head');
+
+    r.response
+     ..write("yeah")
+     ..close();
+  }
+}
